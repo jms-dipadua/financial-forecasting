f17cbafaed024a8d31928c6167f1c6b3d9f8637a
JADALM <james.dipadua@gmail.com>
2016-05-27 07:04:29 +0100

    first attempt at learning curves. testing and head banging to commence. hehe

--- forecast_main.py
+++ forecast_main.py
@@ -220,7 +220,29 @@ class Forecast:
 
 		
 		# visualize results 
-		
+		plt.figure()
+		plt.title(title)
+	    plt.xlabel("Training examples")
+	    plt.ylabel("Score")
+	    train_sizes, train_scores, test_scores = learning_curve(
+	        grid.best_estimator_, self.X_train, self.y_train, cv=2, n_jobs=n_jobs, train_sizes=train_sizes)
+	    train_scores_mean = np.mean(train_scores, axis=1)
+	    train_scores_std = np.std(train_scores, axis=1)
+	    test_scores_mean = np.mean(test_scores, axis=1)
+	    test_scores_std = np.std(test_scores, axis=1)
+	    plt.grid()
+
+	    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+	                     train_scores_mean + train_scores_std, alpha=0.1,
+	                     color="r")
+	    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+	                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+	    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+	             label="Training score")
+	    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+	             label="Cross-validation score")
+
+	    plt.legend(loc="best")
 
 	def ann(self):
 		#print self.company.X_train.shape[1]
