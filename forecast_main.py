86c03eb1bc8d9cfe3cf6d456cb94ee9af026a3d9
JADALM <james.dipadua@gmail.com>
2016-05-28 09:38:27 +0100

    config for v5-2

--- forecast_main.py
+++ forecast_main.py
@@ -66,17 +66,23 @@ class Company:
 		counter = 0
 		# get the columns to keep (manual version)
 		"""
+		drop_cols = []
+		drop_col_nums = []
 		for column in columns:
 			print "Keep (1) or DROP (0):  %r"  % column
 			if int(raw_input()) == 0:
 				drop_cols.append(column)
 				drop_col_nums.append(counter)
 			counter += 1
-		#print drop_cols # so i can keep track of this for experiment documentation purposes 
-		#print drop_col_nums
+		print drop_cols # so i can keep track of this for experiment documentation purposes 
+		print drop_col_nums
 		"""
-		drop_cols = ['DGS10', 'DCOILBRENTEU', 'xCIVPART', 'UNRATE', 'CPIAUCSL', 'GFDEGDQ188S', 'HOUST', 'IC4WSA', 'USD3MTD156N', 'PCE', 'PSAVERT', 'xA191RL1Q225SBEA', 'spClose', 'DEXUSEU', 'EPS', '12mo-EPS', 'net_income', 'total_assets', 'total_revenue', 'free_cash_flow', 'total_liabilities', 'profit_margin']
-		drop_col_nums = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+		# v5-1
+		#drop_cols = ['DGS10', 'DCOILBRENTEU', 'xCIVPART', 'UNRATE', 'CPIAUCSL', 'GFDEGDQ188S', 'HOUST', 'IC4WSA', 'USD3MTD156N', 'PCE', 'PSAVERT', 'xA191RL1Q225SBEA', 'spClose', 'DEXUSEU', 'EPS', '12mo-EPS', 'net_income', 'total_assets', 'total_revenue', 'free_cash_flow', 'total_liabilities', 'profit_margin']
+		#drop_col_nums = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+		# v5-2
+		drop_cols = ['Open', 'High', 'Low', 'SMA-5', 'SMA-15', 'SMA-50', 'SMA-200', 'WMA-10', 'WMA-30', 'WMA-100', 'WMA-200', 'cci-20', 'rsi-14', 'PE']
+		drop_col_nums = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 36]
 		self.raw_data.drop(self.raw_data.columns[drop_col_nums], axis = 1, inplace=True)
 		print list(self.raw_data.columns.values) # again for documentation purposes 
 		
